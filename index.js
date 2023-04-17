204e9102c7e135071094cc6fe9c884da82dee95f
Miguel <mikeguarrochena@hotmail.com>
2023-04-17 19:26:03 -0300

    Change index

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ app.use("/api/auth", require("./routes/auth"));
 app.use("/api/events", require("./routes/events"));
 
 app.get("*", (req, res) => {
+  res.setHeader("Content-Type", "text/javascript");
   res.sendFile(__dirname + "/public/index.html");
 });
 

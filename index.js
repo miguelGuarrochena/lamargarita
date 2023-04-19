9d51faed824efa34c59ac3967d7d42a7c2199dc9
Miguel <mikeguarrochena@hotmail.com>
2023-04-19 16:45:45 -0300

    home

--- index.js
+++ index.js
@@ -19,11 +19,10 @@ app.use(express.static("public"));
 app.use(express.json());
 
 // Rutas
-
+app.use("/api/auth", require("./routes/auth"));
 app.use("/api/events", require("./routes/events"));
 
 app.get("*", (req, res) => {
-  res.setHeader("Content-Type", "text/javascript");
   res.sendFile(__dirname + "/public/index.html");
 });
 

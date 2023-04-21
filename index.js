49e78feb20e6a622e9e0059127a5184dedd8528b
Miguel <mikeguarrochena@hotmail.com>
2023-04-21 14:07:15 -0300

    package.json

--- index.js
+++ index.js
@@ -1,36 +1,3 @@
-// const express = require("express");
-// require("dotenv").config();
-// const cors = require("cors");
-// const { dbConnection } = require("./database/config");
-
-// // Crear el servidor de express
-// const app = express();
-
-// // Base de datos
-// dbConnection();
-
-// // CORS
-// app.use(cors());
-
-// // Directorio Público
-// app.use(express.static("public"));
-
-// // Lectura y parseo del body
-// app.use(express.json());
-
-// // Rutas
-// app.use("/api/auth", require("./routes/auth"));
-// app.use("/api/events", require("./routes/events"));
-
-// app.get("*", (req, res) => {
-//   res.sendFile(__dirname + "/public/index.html");
-// });
-
-// // Escuchar peticiones
-// app.listen(process.env.PORT, () => {
-//   console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
-// });
-
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
@@ -46,15 +13,7 @@ dbConnection();
 app.use(cors());
 
 // Directorio Público
-app.use(
-  express.static("public", {
-    setHeaders: function (res, path, stat) {
-      if (path.endsWith(".js")) {
-        res.set("Content-Type", "application/javascript");
-      }
-    },
-  })
-);
+app.use(express.static("public"));
 
 // Lectura y parseo del body
 app.use(express.json());
